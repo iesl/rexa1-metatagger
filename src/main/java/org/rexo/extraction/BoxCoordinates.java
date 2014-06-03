65e06dd880699c9622ca6e11e4852c4a1124c187
Klim Zaporojets <klimzaporojets@klim.cs.umass.edu>
2014-06-03 16:13:10 -0400

    some coordinates in xml output

---
+++ src/main/java/org/rexo/extraction/BoxCoordinates.java
@@ -0,0 +1,43 @@
+package org.rexo.extraction;
+
+/**
+ * Created by klimzaporojets on 5/29/14.
+ */
+public class BoxCoordinates {
+    private double ury;
+    private double urx;
+    private double lly;
+    private double llx;
+
+    public double getUry() {
+        return ury;
+    }
+
+    public void setUry(double ury) {
+        this.ury = ury;
+    }
+
+    public double getUrx() {
+        return urx;
+    }
+
+    public void setUrx(double urx) {
+        this.urx = urx;
+    }
+
+    public double getLly() {
+        return lly;
+    }
+
+    public void setLly(double lly) {
+        this.lly = lly;
+    }
+
+    public double getLlx() {
+        return llx;
+    }
+
+    public void setLlx(double llx) {
+        this.llx = llx;
+    }
+}
